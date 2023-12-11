c0712788d0188f59127bce657f0cf9912a7e3138
Lucas <pilatilucas@gmail.com>
2023-12-11 13:10:52 -0300

    Update config.js

--- src/axios/config.js
+++ src/axios/config.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-const blogFetch = axios.create({
-    baseURL: "https://jsonplaceholder.typicode.com",
+const blogFetch = axios.create({ //Cria uma instância do Axios com configurações específicas para uma API
+    baseURL: "https://jsonplaceholder.typicode.com", // Define a URL base para todas as requisições feitas com essa instância
     headers: {
-        "Content-Type": "application/json",
+        "Content-Type": "application/json",//indicar que os dados enviados nas requisições serão no formato JSON.Formato de array javascript
     }
 })
 
-export default blogFetch
+export default blogFetch //qualquer requisição feita usando blogFetch terá as configurações padrão
